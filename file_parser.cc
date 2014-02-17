90a42ed975e4b40e5d79f55903f420284edfae01
candaya <christopher.t.andaya@gmail.com>
2014-02-17 15:33:07 -0800

    adding updates

--- file_parser.cc
+++ file_parser.cc
@@ -20,7 +20,18 @@ file_parser::~file_parser() {
 
 file_parser::read_file() {
 //parsing code and storage into vector<struct>
+	if(in.open(in_file_name, ios::in) != 0) {
+		//throw file_parse_exception
+	}
+	while (!in.eof()) {
+		getline(in, parsed_line);
+		file_line.pushback(parse_tokens(line, parsed_line);
+	}
+	in.close();
+}
 
+file_parser::parse_tokens(line fline, string parsed_line_string) {
+	if(parsed_line_string[0] == ' ') //check syntax here for checking column 0
 }
 
 file_parser::get_token(unsigned int line, unsigned int col) {
