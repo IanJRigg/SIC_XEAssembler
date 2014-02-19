3f4124ccc96a4407c66727981af8cc99e7dd35c5
Ricky1833 <Ricky1833@gmail.com>
2014-02-19 15:42:53 -0800

    Modified practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -2,45 +2,55 @@
 #include <cstdlib>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include "file_parse_exception.h"
 
 using namespace std;
 
-void main (int argc, char *argv[]) {
+struct parsed_line{
+	string label;
+	string opcode;
+	string operand;
+	string comment;
+};
+
+int main (int argc, char *argv[]) {
 	ifstream infile;
 	ofstream outfile;
-	vector<string> contents;
 	string line;
+	int i, index, currIndex, v_counter = 0;
+	vector<parsed_line> contents;
+	vector<parsed_line>::iterator v_iter;
 	
 	if (argc != 2) {
 		cout << "Error in argc!" << endl;
-		return;
+		return 1;
 	}
 	
 	infile.open(argv[1], ios::in);
 	if (!infile) {
 		cout << "Error in infile!" << endl;
-		return;
+		return 1;
 	}
 	
 	while (!infile.eof()) {
 		getline(infile, line);
-		contents.pushback(line);
+		contents.push_back(parsed_line());
+		for(unsigned int i=0; i< line.size();i++){
+			if(line[i] == '.'){
+				contents[v_counter].comment = line.substr(i,line.size()-i+1);
+				break;
+			}
+		}
+		v_counter++;
 	}
 	
 	infile.close();
-	
-	for (int i = 0; i < contents.size(); i++) {
-		cout << contents[i] << endl;
+
+	for (unsigned int i=0; i<v_counter;i++) {
+		
+		cout << contents[i].comment << endl;
 	}
-	
-	outfile.open("output.txt", ios::out);
-	if (!outfile) {
-		cout << "Error in outfile!" << endl;
-		return;
-	}
-	
-	for (i = 0; i < contents.size(); i++) {
-		cout << contents[i] << endl;
-	}
-	outfile.close();
+
+	return 0;
 }
