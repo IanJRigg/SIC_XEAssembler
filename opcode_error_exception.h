ea96ba3a97c3bb5a5ceaae90179b40db56ea0212
Ricky1833 <Ricky1833@gmail.com>
2014-02-26 21:40:58 -0800

    Made adjustments to names, typed in all the names of opcodes

--- opcode_error_exception.h
+++ opcode_error_exception.h
@@ -1,38 +1,33 @@
-/*  opcodetab.h
+/*
+    Ian Rigg, Richard Valles, Chris Andaya, Arron Mccarter
+    masc1258
+    opcode_error_exception.h
     CS530, Spring 2014
 */
 
-#ifndef OPCODETAB_H
-#define OPCODETAB_H
+#ifndef OPCODE_ERROR_EXCEPTION_H
+#define OPCODE_ERROR_EXCEPTION_H
+#include <string> 
 
 using namespace std;
 
-class opcodetab {
-    public:
-        // ctor
-        // creates a new dictionary structure and loads all of the opcodes for 
-        // the SIC/XE architecture into the table.  Use the STL map for this.
-        opcodetab(); 
-        
-        // takes a SIC/XE opcode and returns the machine code 
-        // equivalent as a two byte string in hexadecimal.
-        // Example:  get_machine_code("ADD") returns the value 18
-        // Note that opcodes may be prepended with a '+'.
-        // throws an opcode_error_exception if the opcode is not 
-        // found in the table.
-        string get_machine_code(string);  
-        
-        // takes a SIC/XE opcode and returns the number of bytes 
-        // needed to encode the instruction, which is an int in
-        // the range 1..4.
-        // NOTE: the opcode must be prepended with a '+' for format 4.
-        // throws an opcode_error_exception if the opcode is not 
-        // found in the table.        
-        int get_instruction_size(string);
-                        
-    private:
-        // your variables and private methods go here
+class opcode_error_exception {
 
-};
+public:
+    opcode_error_exception(string s) {
+        message = s;        
+        }
+        
+    opcode_error_exception() {
+        message = "An error has occurred";
+        }
+        
+    string getMessage() {
+        return message;
+    }
+    
+private:
+    string message;
+};    
 
 #endif
