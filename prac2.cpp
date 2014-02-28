46ac7f7ac5f50fbc00987a2521f44f4c4e22ae85
candaya <christopher.t.andaya@gmail.com>
2014-02-27 16:14:43 -0800

    working practice model

---
+++ prac2.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "file_parse_exception.h"
+
+using namespace std;
+
+int main() {	
+	std::map<std::string, pair<std::string, std::string> > opcode_map;
+	std::map<std::string, pair<std::string, std::string> >::iterator m_iter;
+	
+	std::string codes[] = { "ADD", "ADDF", "ADDR", "AND", "CLEAR", 
+		"COMP", "COMPF", "COMPR","DIV", "DIVF", "DIVR", "FIX", "FLOAT", "HIO", 
+		"J", "JEQ", "JGT", "JLT", "JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL", 
+		"LDS", "LDT", "LDX", "LPS", "MULF", "MULR", "NORM", "OR", "RD", "RMO", 
+		"RSUB", "SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB", "STCH", "STF",
+		"STI", "STL", "STS", "STSW", "STT", "STX", "SUB", "SUBF", "SUBR",
+		"SVC", "TD", "TIO", "TIX", "TIXR", "WD" };
+	
+	std::string hex_codes[] = { "18", "58", "90", "40", "B4", "28", 
+				"88", "A0", "24", "64", "9C", "C4", "C0", "F4", "3C", 
+				"30", "34", "38", "48", "00","68", "50", "70", "08", 
+				"6C", "74", "04", "D0", "20", "60", "98", "C8", "44", 
+				"D8", "AC", "4C", "A4", "A8", "F0", "EC","0C", "78", 
+				"54", "80", "D4", "14", "7C", "E8", "84", "10",
+				"1C", "5C", "94", "B0", "E0", "F8", "2C", "B8", "DC" };
+	
+	std::string format[] = { "3", "3", "2", "3", "2", "3", "3", "2", "3", 
+				"3", "2", "1", "1", "1", "3", "3", "3", "3", "3", "3", "3", 
+				"3", "3", "3", "3", "3", "3", "3", "3", "3", "2", "1", "3", 
+				"3", "2", "3", "2", "2", "1", "3", "3", "3", "3", "3", "3", 
+				"3", "3", "3", "3", "3", "3", "3", "2", "2", "3", "1", "3", 
+				"2", "3" };
+				
+	cout << codes[1] << endl;
+	
+	for (int i = 0; i < 59; i++) {
+		opcode_map.insert(std::make_pair(codes[i], std::make_pair(hex_codes[i], format[i])));
+	}
+
+	for(m_iter = opcode_map.begin(); m_iter != opcode_map.end(); m_iter++) {
+		cout << "Key: " << m_iter->first << " Value: " << m_iter->second.first << endl;
+	}
+			
+return 0;
+}
+
