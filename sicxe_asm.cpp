82a7daf90144b5654b600ae77c7b9773ebefd78f
Ian Rigg <ianjrigg@gmail.com>
2014-03-19 20:47:12 -0700

    Changes from hex to int counting

--- sicxe_asm.cpp
+++ sicxe_asm.cpp
@@ -11,92 +11,86 @@
 using namespace std;
 
 sicxe_asm::sicxe_asm(string filename){
-  source_file_name = filename;
+  file_parser parser(filename);
+  parser.read_file();
   symtab symbol_table();
   opcodetab opcode_table();
   lines.reserve(500);
-  size = 0;
+  row_num = 0;
 }
 
 sicxe_asm::~sicxe_asm(){}
 
 void sicxe_asm::first_pass(){
-  	file_parser parser(source_file_name);
-  	parser.read_file();
-    string opcode;
-    while((opcode = parser.get_token(size, 1)) != "start"){
-        if(opcode.compare(" ") != 0)
-            throw_error("Command listed before program initialization");
-        write_to_lines("00000", "", "", "");
-        size++;
+    string opcode, location;
+    int location_counter = 0;
+    // Account for EQU statments
+    while((opcode = parser.get_token(row_num, 1)) != "start"){
+         if(opcode.compare(" ") != 0)
+              throw_error("Command listed before program initialization");
+         location = int_to_hex(location_counter);
+         store_line(location, parser.get_token(row_num, 0), opcode, parser.get_token(row_num, 2));
+         row_num++;
     }
-    string location = parser.get_token(size, 2);
-    string location_counter, start_location;
+    location = parser.get_token(row_num, 2); 
     if(location.find("$") != string::npos)
-        location_counter = location.substr(1, string::npos);
+        location_counter = hex_to_int(location.substr(1, string::npos));
     else
-        location_counter = itohs(location);
-    write_to_lines("00000", parser.get_token(size, 0), parser.get_token(size, 1), location);
+        location_counter = hex_to_int(location);
+    location = int_to_hex(location_counter);
+    store_line(location_counter, parser.get_token(row_num, 0), parser.get_token(row_num, 1), parser.get_token(row_num, 2));
     
-    size++;
-    location_counter = validate_address(location_counter);
-    start_location = location_counter;
+    row_num++;
     string label;
     // Account for labels at the beginning
     int opcode_size;
     unsigned int file_size = (unsigned int) parser.size();
-    while(size < file_size){ // Check for "EOF"
-        opcode = parser.get_token(size, 1);
-        label = parser.get_token(size, 0);
+    while(row_num < file_size){ // Check for "EOF"
+        opcode = parser.get_token(row_num, 1);
+        label = parser.get_token(row_num, 0);
         if(opcode.compare(" ") == 0){
-            // if labels are kept here, insert the label into the symtab
-            write_to_lines(location_counter, label, " ", " ");
-            size++;
-            continue;
+           // if labels are kept here, insert the label into the symtab
+           store_line(int_to_hex(location_counter), label, opcode, " ");
+           row_num++;
+           continue;
         }
         if(label.compare(" ") != 0){
             if(symbol_table.already_in_symtab(label))
                 throw_error("Label already exists");
-            symbol_table.insert(label, location_counter);
+            symbol_table.insert(label, int_to_hex(location_counter));
         }
         try{
-            opcode_size = opcode_table.opcode_exists(opcode); // private method
+            opcode_size = opcode_table.get_instruction_size(opcode);
         } catch(opcode_error_exception ex){
             opcode_size = -1;
         }
-        if(opcode_found > 0){
-            opcode_size = opcode_table.get_instruction_size(opcode);
-            location_counter = increment_location_counter(location_counter, opcode_size);
+        if(opcode_size > 0){
+            location_counter += opcode_size;
         }
-        //else if(){ // Handle directives here
-        //}
-        /*else{
-            throw symtab_exception("Uknown command on line: ");
-        }*/
-        write_to_lines(location_counter, label, opcode, parser.get_token(size, 2));
-        size++;
+        else{
+        }
+        store_line(int_to_hex(location_counter), label, opcode, parser.get_token(row_num, 2));
+        row_num++;
     }
     print_file();
 }
 
  //Creates and throws a file_parse_exception
 void sicxe_asm::throw_error(string error){
-    ss_error<<"at line: "<< size + 1 << ", " << error;
+    ss_error<<"at line: "<<row_num + 1 << ", " << error;
     throw symtab_exception(ss_error.str());
 }
-string sicxe_asm::itohs(string dec_string){
+string sicxe_asm::int_to_hex(int num){
     // Converts a decimal integer to a hexadecimal string.
     stringstream stream;
-    stream << dec_string;
-    string temp;
-    stream >> hex >> temp;
-    return temp;
+    stream << hex << num;
+    return validate_address(stream.str());
 }
 
-void sicxe_asm::write_to_lines(string address, string label, string opcode, string operand){
+void sicxe_asm::store_line(string address, string label, string opcode, string operand){
     prog_listing line;
     stringstream stream;
-    stream << size + 1;
+    stream <<row_num + 1;
     line.line_number = stream.str();
     line.address = address;
     line.label = label;
@@ -107,13 +101,30 @@ void sicxe_asm::write_to_lines(string address, string label, string opcode, stri
 
 void sicxe_asm::print_file() {
 //prints the input file in proper format
-    for(unsigned int i = 0; i < size; i++) {
+    for(unsigned int i = 0; i <row_num; i++) {
         cout << lines.at(i).line_number << "\t";
         cout << lines.at(i).address << "\t";
         cout << lines.at(i).label << "\t";
         cout << lines.at(i).opcode << "\t";
         cout << lines.at(i).operand << endl;
     }
+}
+
+bool sicxe_asm::is_hex(string s){
+  int length = s.length();
+  char * hex_string = new char(length);
+  strcpy(hex_string, s.c_str());
+  for(int i = 0; i < length; i++){
+      if(!isxdigit(hex_string[i])) return false;  
+  }
+  return true;
+}
+int sicxe_asm::hex_to_int(string s){
+     if(!is_hex(s))
+         return 0;
+     int value;
+     sscanf(s.c_str(), "%X", &value);
+     return value;
 }
 
 string sicxe_asm::validate_address(string address){
@@ -133,17 +144,6 @@ string sicxe_asm::validate_address(string address){
             throw_error("Starting address is not between 1 and 5 digits");
             return "";
     }
-}
-string sicxe_asm::increment_location_counter(string location_counter, unsigned int opcode_size){
-    unsigned int loc_ctr;
-    stringstream stream;
-    stream << hex << location_counter;
-    stream >> loc_ctr;
-    loc_ctr += size;
-    stream.str("");
-    stream << hex << loc_ctr;
-    cout << loc_ctr << endl;
-    return stream.str();
 }
 
 int main(int argc, char *argv[]){
